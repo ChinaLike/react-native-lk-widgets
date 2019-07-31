a6d33607f9d9efa306e8104670a216000d0ed5bd
ChinaLike <572919350@qq.com>
2019-07-31 16:27:49 +0800

    1.单选组件2.输入组件3.搜索组件

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// import { NativeModules } from 'react-native'
 import Touchable from './lib/Touchable'
 import Button from './lib/Button'
 import SeparatorLine from './lib/SeparatorLine'
 import Image from './lib/Image'
 import Radio from './lib/Radio'
-
-// const { RNWidgets } = NativeModules
+import TextInput from './lib/TextInput'
+import Search from './lib/Search'
 
 module.exports = {
   Touchable,
   Button,
   SeparatorLine,
   Image,
-  Radio
+  Radio,
+  TextInput,
+  Search
 }
